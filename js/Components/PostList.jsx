1bb71b73fb158a95d4610475fe6602642aaad753
plotkopavlo <plotkopavlo@gmail.com>
2018-08-04 15:27:40 +0300

    use data for PostList

--- js/Components/PostList.jsx
+++ js/Components/PostList.jsx
@@ -1,26 +1,26 @@
 import React, { Component } from "react";
 import Post from "./Post";
-//  import preload from "../../data.json";
+import preload from "../../data-blog.json";
 
 class PostList extends Component {
   constructor(props) {
     super(props);
-    this.state = {}  ;
+    this.state = {};
 
     this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
   }
 
   handleSearchTermChange(event) {
-    this.state={}
+    this.state = {};
     console.log(event);
   }
   render() {
     return (
       <section className="post-list">
         <h1 className="post-list__title">Blog List</h1>
-
-        <Post />
-        <Post />
+        {preload.posts.map(post => (
+          <Post {...post} />
+        ))}
       </section>
     );
   }
